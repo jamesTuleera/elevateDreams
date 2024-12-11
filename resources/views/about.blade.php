d9ca9df2340d7ee15f89b655ac5ff666995ac3ae
barituleera <barituleera@gmail.com>
2024-12-11 16:20:33 +0100

    adding content

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,108 +1,119 @@
 @extends('layouts.home')
 
 @section('content')
+    <main id="main">
 
-<main id="main">
-
-    <section class="breadcumb-section" style="background-image: url({{ asset('home_assets/assets/images/page/bg3.jpg') }})">
-        <div class="tf-container" >
-            <div class="row">
-                <div class="col-lg-12 center z-index1">
-                    <h1 class="title">About us</h1>
-                    <ul class="breadcumb-list flex-five">
-                        <li><a href="index.html">Home</a></li>
-                        <li><span>About us
-</span></li>
-                    </ul>
-                    <img class="bcrumb-ab" src="{{ asset('home_assets/assets/images/page/') }}" alt="">
+        <section class="breadcumb-section"
+            style="background-image: url({{ asset('home_assets/assets/images/page/bg3.jpg') }})">
+            <div class="tf-container">
+                <div class="row">
+                    <div class="col-lg-12 center z-index1">
+                        <h1 class="title">About us</h1>
+                        <ul class="breadcumb-list flex-five">
+                            <li><a href="index.html">Home</a></li>
+                            <li><span>About us
+                                </span></li>
+                        </ul>
+                        <img class="bcrumb-ab" src="{{ asset('home_assets/assets/images/page/') }}" alt="">
+                    </div>
                 </div>
+
             </div>
-
-        </div>
-    </section>
+        </section>
 
 
 
-    <!-- Widget Video -->
-    <section class="mt-5 mb-5 about-us-h4" >
-        <div class="tf-container">
-            <div class="row ">
-                <div class="col-lg-12" >
-                    <div class="relative overflow-hidden video-h4-widget mb--14em" style="background-image: url({{ asset('home_assets/assets/images/page/youtubela.jpeg') }})">
-                        <div class="video-wrap">
-                            <a href="https://www.youtube.com/watch?v=n9LgeoJE4EI"
-                                class="relative video-box flex-five z-index3 widget-videos">
-                                <i class="icon-Polygon-4"></i>
-                            </a>
+        <!-- Widget Video -->
+        <section class="mt-5 mb-5 about-us-h4">
+            <div class="tf-container">
+                <div class="row ">
+                    <div class="col-lg-12">
+                        <div class="relative overflow-hidden video-h4-widget mb--14em"
+                            style="background-image: url({{ asset('home_assets/assets/images/page/youtubela.jpeg') }})">
+                            <div class="video-wrap">
+                                <a href="https://www.youtube.com/watch?v=n9LgeoJE4EI"
+                                    class="relative video-box flex-five z-index3 widget-videos">
+                                    <i class="icon-Polygon-4"></i>
+                                </a>
+                            </div>
+                            <P class="relative text-white font-yes center text-video z-index3">Watch Video</P>
                         </div>
-                        <P class="relative text-white font-yes center text-video z-index3">Watch Video</P>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-    <!-- Widget Video -->
-    <!-- Widget About Us -->
-    <section class="about-us-h4">
-        <div class="tf-container">
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="relative image-about-h4-wrap">
+        </section>
+        <!-- Widget Video -->
+        <!-- Widget About Us -->
+        <section class="about-us-h4">
+            <div class="tf-container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="relative image-about-h4-wrap">
 
 
-                        <img class="img-fluid" src="{{ asset('home_assets/assets/images/page/1.jpeg') }}" alt="Image Listing">
+                            <img class="img-fluid" src="{{ asset('home_assets/assets/images/page/1.jpeg') }}"
+                                alt="Image Listing">
+
+                        </div>
+
 
                     </div>
 
-
-                </div>
-
-                <div class="col-md-8 inner-content-about">
+                    <div class="col-md-8 inner-content-about">
 
 
-                    <div class="mb-30">
-                        <h2 class="title-heading mb-18">Welcome to Elevate Dreams
+                        <div class="mb-30">
+                            <h2 class="title-heading mb-18">Welcome to Elevate Dreams
 
-                        </h2>
-                        <p class="des">Welcome to Elevate Dreams, where we empower individuals, entrepreneurs, and visionaries to reach their fullest potential. Our goal is to simplify the path to U.S. immigration and business success, helping exceptional individuals, skilled professionals, and investors achieve their goals. Through personalized support and comprehensive services, we elevate our clients’ dreams and deliver outstanding results.
-
-
-                        </p>
-                    </div>
-
-                    <div class="row">
-                        <div class="mt-2 col-md-12">
-                            <div class="icon-box-style6">
-                                <div class="content">
-                                    <h6 class="title-icon"><a href="#">Our Mission
-
-                                    </a></h6>
-                                    <p class="des-icon">Our mission at Elevate Dreams is to provide clear, personalized pathways for individuals, entrepreneurs, and investors to achieve success in the U.S. Through thorough guidance, tailored strategies, and comprehensive services, we empower our clients to overcome obstacles and reach their goals with confidence. We are dedicated to transforming dreams into tangible results, fostering growth, and creating lasting impact for those we serve.
+                            </h2>
+                            <p class="des">Welcome to Elevate Dreams, where we empower individuals, entrepreneurs, and
+                                visionaries to reach their fullest potential. Our goal is to simplify the path to U.S.
+                                immigration and business success, helping exceptional individuals, skilled professionals,
+                                and investors achieve their goals. Through personalized support and comprehensive services,
+                                we elevate our clients’ dreams and deliver outstanding results.
 
 
-                                    </p>
+                            </p>
+                        </div>
+
+                        <div class="row">
+                            <div class="mt-2 col-md-12">
+                                <div class="icon-box-style6">
+                                    <div class="content">
+                                        <h6 class="title-icon"><a href="#">Our Mission
+
+                                            </a></h6>
+                                        <p class="des-icon">Our mission at Elevate Dreams is to provide clear, personalized
+                                            pathways for individuals, entrepreneurs, and investors to achieve success in the
+                                            U.S. Through thorough guidance, tailored strategies, and comprehensive services,
+                                            we empower our clients to overcome obstacles and reach their goals with
+                                            confidence. We are dedicated to transforming dreams into tangible results,
+                                            fostering growth, and creating lasting impact for those we serve.
+
+
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div class="mt-5 col-md-12">
+                                <div class="icon-box-style6">
+                                    <div class="content">
+                                        <h6 class="title-icon"><a href="#">Our Vision
+
+                                            </a></h6>
+                                        <p class="des-icon">We envision a world where crossing borders to pursue a better
+                                            future or grow a business is a seamless process, filled with trust, opportunity,
+                                            and hope. At Elevate Dreams, we open doors for individuals and businesses alike,
+                                            turning aspirations into reality.
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-
-
-                        <div class="mt-5 col-md-12">
-                            <div class="icon-box-style6">
-                                <div class="content">
-                                    <h6 class="title-icon"><a href="#">Our Vision
-
-                                    </a></h6>
-                                    <p class="des-icon">We envision a world where crossing borders to pursue a better future or grow a business is a seamless process, filled with trust, opportunity, and hope. At Elevate Dreams, we open doors for individuals and businesses alike, turning aspirations into reality.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </section>
-    <!-- Widget About Us -->
-
-
-
-@endsection
+        </section>
+        <!-- Widget About Us -->
+    @endsection
---
+++ resources/views/our-team.blade.php
@@ -0,0 +1,266 @@
+@extends('layouts.home')
+@section('content')
+
+
+<section class="breadcumb-section"
+style="background-image: url({{ asset('home_assets/assets/images/page/bg3.jpg') }})">
+<div class="tf-container">
+    <div class="row">
+        <div class="col-lg-12 center z-index1">
+            <h1 class="title">Meet Our Team</h1>
+            <ul class="breadcumb-list flex-five">
+                <li><a href="index.html">Home</a></li>
+                <li><span>Meet Our Team
+                    </span></li>
+            </ul>
+            <img class="bcrumb-ab" src="{{ asset('home_assets/assets/images/page/') }}" alt="">
+        </div>
+    </div>
+
+</div>
+</section>
+
+<section class="team-member-page pd-main">
+    <div class="tf-container">
+        <div class="mb-40 row">
+            <div class="col-lg-12">
+                <div class="center m0-auto w-text-heading">
+                    <span class="sub-title-heading text-main font-yes fs-28-46">Populer
+                        Activities</span>
+                    <h2 class="title-heading ">meet our experienced <span
+                            class="text-main font-yes">Team</span> people</h2>
+                </div>
+            </div>
+        </div>
+        <div class="team-member-grid">
+            <div class="tf-widget-team">
+                <div class="team-image mb-15">
+                    <img src="./assets/images/team/team1.jpg" alt="">
+                    <ul class="social-team">
+                        <li>
+                            <a href="#">
+                                <i class="icon-9"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="icon-5"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="icon-linkedin"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="icon-youtube"></i>
+                            </a>
+                        </li>
+                    </ul>
+
+                </div>
+                <div class="team-content center">
+                    <p class="job">SR. Tour Manager</p>
+                    <h4 class="name"><a href="#">Leslie Alexander</a></h4>
+                </div>
+            </div>
+            <div class="tf-widget-team">
+                <div class="team-image mb-15">
+                    <img src="./assets/images/team/team2.jpg" alt="">
+                    <ul class="social-team">
+                        <li>
+                            <a href="#">
+                                <i class="icon-9"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="icon-5"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="icon-linkedin"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="icon-youtube"></i>
+                            </a>
+                        </li>
+                    </ul>
+
+                </div>
+                <div class="team-content center">
+                    <p class="job">SR. Tour Manager</p>
+                    <h4 class="name"><a href="#">Leslie Alexander</a></h4>
+                </div>
+            </div>
+            <div class="tf-widget-team">
+                <div class="team-image mb-15">
+                    <img src="./assets/images/team/team3.jpg" alt="">
+                    <ul class="social-team">
+                        <li>
+                            <a href="#">
+                                <i class="icon-9"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="icon-5"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="icon-linkedin"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="icon-youtube"></i>
+                            </a>
+                        </li>
+                    </ul>
+
+                </div>
+                <div class="team-content center">
+                    <p class="job">SR. Tour Manager</p>
+                    <h4 class="name"><a href="#">Leslie Alexander</a></h4>
+                </div>
+            </div>
+            <div class="tf-widget-team">
+                <div class="team-image mb-15">
+                    <img src="./assets/images/team/team4.jpg" alt="">
+                    <ul class="social-team">
+                        <li>
+                            <a href="#">
+                                <i class="icon-9"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="icon-5"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="icon-linkedin"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="icon-youtube"></i>
+                            </a>
+                        </li>
+                    </ul>
+
+                </div>
+                <div class="team-content center">
+                    <p class="job">SR. Tour Manager</p>
+                    <h4 class="name"><a href="#">Leslie Alexander</a></h4>
+                </div>
+            </div>
+            <div class="tf-widget-team">
+                <div class="team-image mb-15">
+                    <img src="./assets/images/team/team5.jpg" alt="">
+                    <ul class="social-team">
+                        <li>
+                            <a href="#">
+                                <i class="icon-9"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="icon-5"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="icon-linkedin"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="icon-youtube"></i>
+                            </a>
+                        </li>
+                    </ul>
+
+                </div>
+                <div class="team-content center">
+                    <p class="job">SR. Tour Manager</p>
+                    <h4 class="name"><a href="#">Leslie Alexander</a></h4>
+                </div>
+            </div>
+            <div class="tf-widget-team">
+                <div class="team-image mb-15">
+                    <img src="./assets/images/team/team6.jpg" alt="">
+                    <ul class="social-team">
+                        <li>
+                            <a href="#">
+                                <i class="icon-9"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="icon-5"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="icon-linkedin"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="icon-youtube"></i>
+                            </a>
+                        </li>
+                    </ul>
+
+                </div>
+                <div class="team-content center">
+                    <p class="job">SR. Tour Manager</p>
+                    <h4 class="name"><a href="#">Leslie Alexander</a></h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="brand-logo-widget bg-4">
+    <div class="tf-container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="overflow-hidden swiper brand-logo">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <img src="./assets/images/page/brand-logo.png" alt="">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="./assets/images/page/brand-logo.png" alt="">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="./assets/images/page/brand-logo.png" alt="">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="./assets/images/page/brand-logo.png" alt="">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="./assets/images/page/brand-logo.png" alt="">
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="./assets/images/page/brand-logo.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+
+
+@endsection
